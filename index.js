33a8c596cc54ccbebe8710a466881ab4f564646f
Eamon Sheil <Eamondsheil@gmail.com>
2022-02-24 17:17:40 -0500

    commit tree e15725b3078f5ff3809aa5a5b52eda8325fe9094

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Write your solution in this file!
+let employee = {
+    name: "Eamon", streetAddress: "10 Broadway" 
+}
+
+function updateEmployeeWithKeyAndValue(obj, key, value) {
+    const newObj = {...obj}
+    newObj[key] = value;
+    return newObj;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value) {
+    obj[key] = value;
+    return obj;
+}
+
+function deleteFromEmployeeByKey(obj, key) {
+    const newObj = {...obj}
+    delete newObj[key];
+    return newObj;
+
+}
+
+function destructivelyDeleteFromEmployeeByKey(obj, key) {
+delete obj[key];
+return obj;
+}
